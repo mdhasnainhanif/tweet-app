dcfd8398a15ce0dad89b946d6d392b50cb0fd3b4
Anas Ansari <121580678+annasansari@users.noreply.github.com>
2024-05-22 03:11:23 +0500

    working anas

--- backend/controllers/tweet.js
+++ backend/controllers/tweet.js
@@ -1,47 +1,42 @@
 const Tweet = require("../models/Tweet.js");
 
 exports.createTweet = async (req, res, next) => {
-
-    const { content } = req.body
+    const { content } = req.body;
     try {
         if (!content) {
-            res.status(400).json({ message: "Content is required!" })
+            return res.status(400).json({ message: "Content is required!" });
         }
-        const tweet = new Tweet({ content })
-        await tweet.save()
-        res.status(201).json({ message: "Tweet created successfully", data: tweet })
+        const tweet = new Tweet({ content });
+        await tweet.save();
+        return res.status(201).json({ message: "Tweet created successfully", data: tweet });
     } catch (error) {
-        res.status(500).json({ error: error.message })
+        return res.status(500).json({ error: error.message });
     }
 };
-
 
 exports.deleteTweet = async (req, res, next) => {
     try {
-        const { id } = req.params
-        const tweet = await Tweet.findByIdAndDelete(id)
+        const { id } = req.params;
+        const tweet = await Tweet.findByIdAndDelete(id);
         if (!tweet) {
-            res.status(404).json({ message: "Tweet not found" })
+            return res.status(404).json({ message: "Tweet not found" });
         }
-        res.status(200).json({ mesage: "Tweet Deleted successfully" })
+        return res.status(200).json({ message: "Tweet deleted successfully" });
     } catch (error) {
-        res.status(500).json({ error: error.message })
+        return res.status(500).json({ error: error.message });
     }
 };
 
-
 exports.getAllTweets = async (req, res, next) => {
-
     try {
-        const tweets = await Tweet.find()
+        const tweets = await Tweet.find();
+        console.log(tweets);
 
-        if (tweets.length == 0 || !tweets) {
-            res.status(403).json({ message: "Tweets not found" })
+        if (tweets.length < 1) {
+            return res.status(403).json({ message: "Tweets not found" });
         }
-        res.status(200).json({ data: tweets })
-
+        return res.status(200).json({ data: tweets });
     } catch (error) {
-        res.status(500).json({ error: error.message })
-
+        return res.status(500).json({ error: error.message });
     }
-}
+};
